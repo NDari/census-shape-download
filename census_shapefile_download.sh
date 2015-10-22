e2df4c42fd6be4edbaff481eddc5023f5d8dd1ba
I-Kang Ding <ikang.ding@capitalone.com>
2015-10-21 23:35:41 -0400

    Add census tract shapefiles for NY and CA

--- census_shapefile_download.sh
+++ census_shapefile_download.sh
@@ -2,10 +2,10 @@
 # This shell script will run the census_shapefile_download.py to download shape files from census bureau site.
 # Modify the arguments in for-loop or the python call itself to suit what you need.
 
-for g in bg tract
+for g in tract
 do
-    for y in 2010 2011 2012 2013 2014
+    for y in 2010 2011 2012 2013
     do
-        python census_shapefile_download.py --year $y --geolevel $g --s DC MD VA --json
+        python census_shapefile_download.py --year $y --geolevel $g --s CA NY --json
     done
 done
